93e162c5176668c2eec05ddefd3de041a98af6d8
Alan Mathias <105767747+AlanthiasCO@users.noreply.github.com>
2023-02-11 00:04:47 -0300

    Update InMemoryDatabase.java

--- src/main/java/br/ufpr/mural/database/InMemoryDatabase.java
+++ src/main/java/br/ufpr/mural/database/InMemoryDatabase.java
@@ -1,187 +1,203 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.ufpr.mural.database;
 
+package br.ufpr.mural.server;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
-import br.ufpr.mural.core.Anuncio;
-import br.ufpr.mural.core.Comentario;
-import br.ufpr.mural.core.Evento;
-import br.ufpr.mural.core.Mural;
-import br.ufpr.mural.core.Post;
-import br.ufpr.mural.core.Reacao;
-import br.ufpr.mural.core.Sugestao;
-import br.ufpr.mural.core.Usuario;
+import br.ufpr.mural.core.mural.Anuncio;
+import br.ufpr.mural.core.mural.Comentario;
+import br.ufpr.mural.core.mural.Evento;
+import br.ufpr.mural.core.mural.Lembrete;
+import br.ufpr.mural.core.mural.Mural;
+import br.ufpr.mural.core.mural.Post;
+import br.ufpr.mural.core.mural.Reacao;
+import br.ufpr.mural.core.mural.Sugestao;
+import br.ufpr.mural.core.usuario.Usuario;
 
-/**
- *
- * @author helio
- */
-public class InMemoryDatabase implements DatabaseDao {  // DAO = Data Access Object
-        
-   
-	private Map<String, Usuario> usuarios;  // agregação 1 para muitos
-//	private Map<String, Mural> murais;
+
+public class InMemoryDatabase implements DatabaseDao{ // DAO = Data Access Object
+
+	private Map<String, Usuario> usuarios; // agregação 1 para muitos
+	private Map<String, Mural> murais;
 	
 
-    public InMemoryDatabase() {
+	public InMemoryDatabase() {
 		this.usuarios = new HashMap<>();
-        // TODO atribuir map de murais
+		this.murais = new HashMap<>();
+
 	}
 	
-    public void inserirUsuario(Usuario usuario){
-        this.usuarios.put(usuario.getUserName(), usuario);
-    }
-    
-    public Usuario getUsuario(String userName) {
-    	return this.usuarios.get(userName);
-    }
-
-    /**
-     * Limpa os maps.
-     */
 	@Override
-	public void limparBase() {
+	public void limparBanco() {
 		this.usuarios = new HashMap<>();
-        // TODO: limpar murais
-        // RESETANDO GERADORES DE ID:
-        Usuario.resetaIds();
-        // TODO: resetar ids de todas as classes
+		this.murais = new HashMap<>();
+		Usuario.resetaIds();
+		Post.resetaIdPost();
 	}
 
-    @Override
-    public void inserirComentario(Comentario comentario, Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	// USUARIOS
+	public void inserirUsuario(Usuario usuario) {
+		this.usuarios.put(usuario.getUserName(), usuario);
+	}
 
-    @Override
-    public void inserirConfirmacaoEvento(Evento evento, Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	public Usuario getUsuario(String userNameNovo) {
+		return this.usuarios.get(userNameNovo);
 
-    @Override
-    public void inserirLembrete(Lembrete lembrete, Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	}
 
-    @Override
-    public void inserirMural(Mural mural) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	// MURAIS
+	public void criarMural(Mural mural) {
+		this.murais.put(mural.getNome(), mural);
+	}
 
-    @Override
-    public void inserirPost(Evento post, Mural mural) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	public Mural getMural(String nomeMural) {
+		return this.murais.get(nomeMural);
+	
+	}
 
-    @Override
-    public void inserirPost(Anuncio post, Mural mural) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public void inserirPostSalvo(Post post, Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public void inserirReacao(Reacao reacao, Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public void inserirSugestao(Sugestao sugestao) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public List<Comentario> recuperarComentarios(Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Evento> recuperarEventosConfirmados(Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Lembrete> recuperarLembretes(Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public Mural recuperarMural(int idMural) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public void recuperarPosts(Mural mural) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public List<Post> recuperarPostsSalvos(Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Reacao> recuperarReacoes(Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Sugestao> recuperarSugestoesFeitas(Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Sugestao> recuperarSugestoesPost(Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public List<Sugestao> recuperarSugestoesRecebidas(Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        return null;
-    }
-
-    @Override
-    public void removerConfirmacaoEvento(Evento evento, Usuario usuario) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
-
-    @Override
-    public void removerReacao(Reacao reacao, Post post) {
-        // NÃO PRECISA IMPLEMENTAR
-        
-    }
+	@Override
+	public Map<String, Mural> listMurais() {
+		return this.murais;
+	}
 
 	
-    
-    
-    
+	
+	//OBRIGATORIOS DEVIDO O IMPLEMENTS
+	
+	@Override
+	public void inserirMural(Mural mural) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mural recuperarMural(int idMural) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void recuperarPosts(Mural mural) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserirPost(Evento post, Mural mural) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserirPost(Anuncio post, Mural mural) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserirComentario(Comentario comentario, Post post) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Comentario> recuperarComentarios(Post post) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserirPostSalvo(Post post, Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Post> recuperarPostsSalvos(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserirReacao(Reacao reacao, Post post) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Reacao> recuperarReacoes(Post post) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removerReacao(Reacao reacao, Post post) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserirConfirmacaoEvento(Evento evento, Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Evento> recuperarEventosConfirmados(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removerConfirmacaoEvento(Evento evento, Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserirLembrete(Lembrete lembrete, Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lembrete> recuperarLembretes(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserirSugestao(Sugestao sugestao) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Sugestao> recuperarSugestoesRecebidas(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Sugestao> recuperarSugestoesFeitas(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Sugestao> recuperarSugestoesPost(Post post) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removerPost(int idPost) {
+		// TODO Auto-generated method stub
+		
+	}
 }
