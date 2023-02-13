d69759a14c9a3968f38b470b875d422bad9382b8
alanc <alanc@10.158.248.50>
2023-02-13 15:19:12 -0300

    commit de teste

--- src/main/java/br/ufpr/mural/util/Util.java
+++ src/main/java/br/ufpr/mural/util/Util.java
@@ -3,6 +3,8 @@ package br.ufpr.mural.util;
 
 import java.util.List;
 
+//testando o git - criando apenas para verificar se está funcionado a conexcao do git com
+// o projeto no eclipse
 
 public class Util {
 
